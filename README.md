44ce6b97d1c73098f047d8a256ceefb3f80f3d00
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-28 17:54:51 +0530

    setup done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 1. first created the components folder
 2. then created the card.jsx component
 3. determined all the properties like className,variant,hover,padding,children and passed them as props
-4. created the base styles
+4. created the base styles,these apply to all cards no matter what
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,8 +5,14 @@ const Card = ({
   hover = true,
   padding = "normal",
 }) => {
+  const baseStyles = "rounded-lg shadow-md transition-all duration-300";
 
-  const baseStyles=""
+  const variants = {
+    default: "bg-white border border-gray-200",
+    primary: "bg-blue-50 border border-blue-200 ",
+    success: "bg-green-50 border border-blue-200",
+    dark: "bg-gray-800 text-white border-gray-700",
+  };
 
   return <div></div>;
 };
